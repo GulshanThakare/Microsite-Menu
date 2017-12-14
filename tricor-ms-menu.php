a673b12f4bc6227cfbf87d1fc29b088b367f80f6
Gulshan Thakare <gulshan@2020compute.com>
2017-12-14 11:52:15 +0530

    new walker

--- tricor-ms-menu.php
+++ tricor-ms-menu.php
@@ -7,6 +7,57 @@ Version: 1.0
 Author: Gulshan Thakare
 Author URI: http://www.2020compute.com/
 */
+class Wpse8170_Menu_Walker extends Walker_Nav_Menu {
+
+    var $number = 1;
+
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $class_names = $value = '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes[] = 'menu-item-' . $item->ID;
+
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+        // add span with number here
+        if ( $depth == 0 ) { // remove if statement if depth check is not required
+            $output .= sprintf( '<span>%02s.</span>', $this->number++ );
+        }
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
+}
 class subMenu extends Walker_Nav_Menu {
     function end_el(&$output, $item, $depth=0, $args=array()) {
 
@@ -37,8 +88,8 @@ function print_menu_shortcode($atts, $content = null) {
 		'after'           => '',
 		'link_before'     => '',
 		'link_after'      => '',
-		'depth'           => 1,
-		'walker'          => new subMenu,
+		'depth'           => 2,
+		'walker'          => new Wpse8170_Menu_Walker,
 		'theme_location'  => ''));
 		
 		return $output;
